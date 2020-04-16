67ed5ef3d351fcf6fbb8598cb7c9161c3bc1f408
Petrov Aleksandr <rpm10b@gmail.com>
2020-04-16 13:28:50 +0300

    Сделал таски по модулю 1

---
+++ task-2/task-2.js
@@ -0,0 +1,24 @@
+const total = 100;
+let order = 50;
+
+if (order > total) {
+  console.log('На складе недостаточно твоаров!');
+} else {
+  console.log('Заказ оформлен, с вами свяжется менеджер');
+}
+
+order = 80;
+
+if (order > total) {
+  console.log('На складе недостаточно твоаров!');
+} else {
+  console.log('Заказ оформлен, с вами свяжется менеджер');
+}
+
+order = 120;
+
+if (order > total) {
+  console.log('На складе недостаточно твоаров!');
+} else {
+  console.log('Заказ оформлен, с вами свяжется менеджер');
+}
